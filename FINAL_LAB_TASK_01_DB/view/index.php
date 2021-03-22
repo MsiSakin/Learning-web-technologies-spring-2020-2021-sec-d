79aaa033a91575abb33a2a05d377a7d72b78a437
MsiSakin <msisakin00@gmail.com>
2021-03-22 10:45:12 +0600

    Create Done

---
+++ FINAL_LAB_TASK_01_DB/view/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Landing pange</title>
+</head>
+<body>
+
+	<center>
+		<a href="login.php"> Login</a> | 
+		<a href="reg.php"> Signup</a>  
+
+	</center>
+</body>
+</html>
